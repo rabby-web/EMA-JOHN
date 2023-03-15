f114de67a35b12f8f34affa0add3e798e4de3ff3
Rabby <rabby.webdeveloper@gmail.com>
2023-03-15 08:36:50 +0600

    ema-jhon start

---
+++ src/components/Product/Product.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const Product = (props) => {
+    console.log(props)
+    const {product, handleAddToCart} = props;
+    const {name, img, seller, price, ratings} = product;
+    return (
+        <div className='product'>
+            <img src={img} alt=""/>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p className='product-price'>Price: ${price}</p>
+                <p className='product-seller'>Menufecture: {seller}</p>
+                <p className='product-rating'><small>Rating: {ratings}</small></p>
+            </div>
+            <button onClick={() => handleAddToCart(product)} className='btn-cart'>
+                <p className='btn-text'>Add to Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
