fb710e721537e1224d5450cc54ccbb734ee09dcd
Rabby <rabby.webdeveloper@gmail.com>
2023-03-16 12:05:31 +0600

    my fast commit

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
-    console.log(props)
+    // console.log(props)
     const {product, handleAddToCart} = props;
     const {name, img, seller, price, ratings} = product;
     return (
