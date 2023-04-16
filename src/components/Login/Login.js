8919ddd5793552e2d48c9e905ee16c6579215b08
Rabby <rabby.webdeveloper@gmail.com>
2023-04-16 20:41:57 +0600

    login section

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
+            <h1>This is Sign Section</h1>
             <h2>This is login section</h2>
         </div>
     );
