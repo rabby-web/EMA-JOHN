3f48aa7174c55f614425e5c699f926f6a055cbb5
Rabby <rabby.webdeveloper@gmail.com>
2023-04-16 22:00:58 +0600

    logi title

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,6 +5,12 @@ const Login = () => {
     return (
         <div className='form-container'>
             <h1 className='form-title'>Login</h1>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="" />
+                </div>
+            </form>
         </div>
     );
 };
