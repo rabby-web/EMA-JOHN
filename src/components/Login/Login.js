0f206dd029e793b5c51397b8359b09aa93e5830c
Rabby <rabby.webdeveloper@gmail.com>
2023-04-16 22:58:10 +0600

    logi inpute complete

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Login.css'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
@@ -8,9 +9,15 @@ const Login = () => {
             <form>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="" />
+                    <input type="email" name="email" id="" required/>
                 </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="" required/>
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
             </form>
+            <p>New to ema john <Link to='/signup'>Create New Account</Link></p>
         </div>
     );
 };
